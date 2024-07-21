20c7adf9553f99cd3194a6f3111794b16d169fd1
BURNSIDE HIGH SCHOOL <code@burnside.school.nz>
2024-07-22 09:57:08 +1200

    Jesus

--- prefabs/bubbles.gd
+++ prefabs/bubbles.gd
@@ -1,4 +1,4 @@
-extends Button
+extends TileMap
 
 
 # Called when the node enters the scene tree for the first time.
@@ -7,9 +7,7 @@ func _ready():
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
 
-
-func _on_pressed():
-	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
+func _on_body_entered(body):
+	if body.name == "player":
+		get_tree().change_scene_to_file("res://scenes/dead.tscn")	
--- scripts/player.gd
+++ scripts/player.gd
@@ -9,8 +9,6 @@ var start_position = Vector2(600, 250)
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 
-@onready var anim : AnimatedSprite2D = $AnimatedSprite2D
-
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
@@ -19,7 +17,6 @@ func _physics_process(delta):
 	# Handle jump.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
-		anim.play("jumo")
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
@@ -36,6 +33,4 @@ func _physics_process(delta):
 		position = start_position
 
 
-func player(body):
-	if body.name == "Bubbles":
-		get_tree().change_scene_to_file("res://scenes/dead.tscn")
+

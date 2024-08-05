a9de8e3f8311ae03b54203df4c6eaf97081ba0a8
BURNSIDE HIGH SCHOOL <163483360+jack-likes-carrots@users.noreply.github.com>
2024-08-06 11:46:49 +1200

    level 2 and double jump

--- scripts/player.gd
+++ scripts/player.gd
@@ -3,6 +3,9 @@ extends CharacterBody2D
 
 const SPEED = 400.0
 const JUMP_VELOCITY = -700.0
+#double jumping
+var jump_count = 0
+var max_jumps = 2
 
 var start_position = Vector2(600, 250)
 
@@ -14,9 +17,13 @@ func _physics_process(delta):
 	if not is_on_floor():
 		velocity.y += gravity * delta
 
+	if is_on_floor():
+		jump_count = 0
+	
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("ui_accept") and jump_count < max_jumps:
 		velocity.y = JUMP_VELOCITY
+		jump_count += 1
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.

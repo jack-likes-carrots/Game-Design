dde12227faf22404e4b78ec5f2922cd813bdd026
jack-likes-carrots <163483360+jack-likes-carrots@users.noreply.github.com>
2024-08-20 14:13:06 +1200

    quick fix

--- scenes/button.gd
+++ scenes/button.gd
@@ -14,4 +14,4 @@ func _process(delta: float) -> void:
 
 
 func _on_pressed():
-	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
+	get_tree().change_scene_to_file("res://scenes/level_2.tscn")

e061f285cf9f91f9f5505eeae6e2a7318e046335
jack-likes-carrots <163483360+jack-likes-carrots@users.noreply.github.com>
2024-09-11 21:45:23 +1200

    couple of things :)

--- prefabs/enemy_lvl2.gd
+++ prefabs/enemy_lvl2.gd
@@ -1,12 +1,14 @@
-extends Node2D
+extends Area2D
 
-@onready var path_follow : PathFollow2D = $Path2D/PathFollow2D
-#pixels per second
-@export var speed = 500
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	path_follow.progress += speed * delta
+	pass
 
 
 func _on_body_entered(body):
---
+++ timer.gd
@@ -0,0 +1,10 @@
+extends Control
+
+func _process(_delta):
+	var time: float = Node2D.time
+	var msec = fmod(time, 1) * 100
+	var seconds = fmod(time, 60)
+	var minutes = fmod(time, 3600) / 60
+	$Minutes.text =  "[b]%02d:[/b]" % minutes
+	$Seconds.text =  "[b]%02d.[/b]" % seconds
+	$Msec.text =  "[b]%03d[/b]" % msec
